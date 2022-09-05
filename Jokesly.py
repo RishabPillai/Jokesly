bfa0226f4949e6693caa1220b414c3cfc905edd2
RishabPillai <112868582+RishabPillai@users.noreply.github.com>
2022-09-05 20:47:53 +0530

    Update Jokesly.py

--- Jokesly.py
+++ Jokesly.py
@@ -4,9 +4,16 @@ jokes = ["What’s the best thing about Switzerland I don’t know, but the flag
          "Did you hear about the mathematician who’s afraid of negative number He’ll stop at nothing to avoid them."
          ,"Why do we tell actors to “break a leg? Because every play has a cast. "]
 
-def getjoke():
+def get_joke():
     joke = random.choice(jokes)
     print(joke)
 
 def alljoke():
     print(jokes)
+    
+def add_joke(new_joke):
+    jokes.append(new_joke)
+
+def remove_joke(remove_joke):
+    jokes.remove(remove_joke)
+
